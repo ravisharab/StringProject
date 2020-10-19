bff6b5d27368ef5bfc67b8bd1e25c69acda36f2c
Ravi Ganta <ravisharab@gmail.com>
2020-10-19 11:13:24 -0400

    added two methods

--- StringProject/src/com/String/StringClassAll.java
+++ StringProject/src/com/String/StringClassAll.java
@@ -1,9 +1,22 @@
 package com.String;
 
+import java.util.stream.IntStream;
+
 public class StringClassAll {
 
 	public static void main(String[] args) {
 		System.out.println("String class");
+		
+		//at 2 character the output is r
+		String strName = "BankOfAmerica";
+		char at = strName.charAt(2);
+		System.out.println(at);
+		
+		//Int Stream is deprecated i think
+		IntStream chars = strName.chars();
+		System.out.println(chars);
+		
+		//commits not visible in github desktop
 
 	}
 
